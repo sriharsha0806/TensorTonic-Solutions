24cf99c5f91c328352b9f860e95ae4e705f1117e
sriharsha annamaneni <sriharsha0806@gmail.com>
2026-05-29 23:22:35 +0530

    Add solution for cosine-similarity

---
+++ cosine-similarity/cosine-similarity.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def cosine_similarity(a, b):
+    """
+    Compute cosine similarity between two 1D NumPy arrays.
+    Returns: float in [-1, 1]
+    """
+    # Write code here
+    a, b = np.asarray(a), np.asarray(b)
+    norm_a, norm_b = np.linalg.norm(a), np.linalg.norm(b)
+    if norm_a == 0 or norm_b == 0:
+        return 0
+    return np.dot(a,b)/(norm_a*norm_b)
