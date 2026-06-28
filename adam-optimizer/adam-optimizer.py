adc968f314f3d1e40971ddb16b23c1bc9356c2fc
sriharsha annamaneni <sriharsha0806@gmail.com>
2026-06-28 20:31:41 +0530

    Solve adam-optimizer

--- adam-optimizer/adam-optimizer.py
+++ adam-optimizer/adam-optimizer.py
@@ -5,17 +5,11 @@ def adam_step(param, grad, m, v, t, lr=1e-3, beta1=0.9, beta2=0.999, eps=1e-8):
     One Adam optimizer update step.
     Return (param_new, m_new, v_new).
     """
-    param = np.asarray(param, dtype=float)
-    grad = np.asarray(grad, dtype=float)
-
-    m, v = np.asarray(m, dtype=float), np.asarray(v, dtype=float)
+    # Write code here
+    param, grad, m, v = np.asarray(param, dtype=float), np.asarray(grad, dtype=float), np.asarray(m, dtype=float), np.asarray(v, dtype=float)
+    m_updated = beta1*m + (1-beta1)*grad 
+    v_updated = beta2*v + (1-beta2)*grad**2
+    m_updated_bias_correction, v_updated_bias_correction = m_updated/(1-beta1**t), v_updated/(1-beta2**t)
+    param_updated = param - lr*(m_updated_bias_correction)/(np.sqrt(v_updated_bias_correction)+eps)
+    return param_updated, m_updated, v_updated
     
-    # Update First Moment
-    m_new = beta1*m + (1-beta1)*grad 
-    # Update second moment 
-    v_new = beta2*v + (1-beta2)*grad**2
-    # Bias Correction
-    m_hat = m_new/(1-beta1**t)
-    v_hat = v_new/(1-beta2**t)
-    param_new = param - lr*m_hat/(np.sqrt(v_hat) + eps)
-    return param_new, m_new, v_new
